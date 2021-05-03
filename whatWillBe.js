4b6b61987f3f123e054df022558923fbc5a89eb5
NAREK KARAPETYAN <krptnrkrptnrk@gmail.com>
2021-05-03 18:35:20 +0400

    add README.md and moified whatWillBe.js

--- whatWillBe.js
+++ whatWillBe.js
@@ -1,17 +1,17 @@
-// console.log("" + 1 + 0); //10 //qani vor operandneric meky string a,bolory kdarnan string, u kkpnen irar
-// console.log("" - 1 + 0); //-1 //qani vor arajin gorcoxutyuny -n a, stringy sarqum a number /0/, u katarum a gorcoxutyunnery 0 - 1 + 0 = -1
-// console.log(true + false); //1 //sarqum a number, gumarum a // 1 + 0 = 1
-// console.log(6 / "3"); //3 //"3"-y sarqum a number
-// console.log("2" + "3"); //23 //gumarum a, mejy ka string, aysinqn katarum a stringneri miacum
-// console.log(4 + 5 + "px"); //9px //skzbic katarum a arajin 2 operandneri(numbernei gumarumy), heto stacvacin gumarum a string, aysinqn mianum en
-// console.log("$" + 4 + 5); //$45 //"$"in miacnum a "4"y, heto stacvac stringin el miacnum a "5"y
-// console.log("4" - 2); //2 //- a, "4"y sarqum a number 4, u hanum 2
-// console.log("4px" - 2); //NaN //- a, string "4px"-y porcum a sarqi number, bayc qani vor mejy tarer kan, stanum a NotANumber
-// console.log(7 / 0); //Infinity //zut tivy 0i bajanac Infinity a tali
-// console.log(" -9 " + 5); // -9 5 //pastoren dimaci u hetevi spaceery avtomat chi maqrum
-// console.log(" -9 " - 5); //-14 // " -9 "-ic vercrec number -9-y, u -9-5=-14
-// console.log(null + 1); //1 //null-y sarqum a number` 0, u 0+1=1
-// console.log(undefined + 1); //NaN //undefinedy number piti sarqi, talis a NaN
+console.log("" + 1 + 0); //10 //qani vor operandneric meky string a,bolory kdarnan string, u kkpnen irar
+console.log("" - 1 + 0); //-1 //qani vor arajin gorcoxutyuny -n a, stringy sarqum a number /0/, u katarum a gorcoxutyunnery 0 - 1 + 0 = -1
+console.log(true + false); //1 //sarqum a number, gumarum a // 1 + 0 = 1
+console.log(6 / "3"); //3 //"3"-y sarqum a number
+console.log("2" + "3"); //23 //gumarum a, mejy ka string, aysinqn katarum a stringneri miacum
+console.log(4 + 5 + "px"); //9px //skzbic katarum a arajin 2 operandneri(numbernei gumarumy), heto stacvacin gumarum a string, aysinqn mianum en
+console.log("$" + 4 + 5); //$45 //"$"in miacnum a "4"y, heto stacvac stringin el miacnum a "5"y
+console.log("4" - 2); //2 //- a, "4"y sarqum a number 4, u hanum 2
+console.log("4px" - 2); //NaN //- a, string "4px"-y porcum a sarqi number, bayc qani vor mejy tarer kan, stanum a NotANumber
+console.log(7 / 0); //Infinity //zut tivy 0i bajanac Infinity a tali
+console.log(" -9 " + 5); // -9 5 //pastoren dimaci u hetevi spaceery avtomat chi maqrum
+console.log(" -9 " - 5); //-14 // " -9 "-ic vercrec number -9-y, u -9-5=-14
+console.log(null + 1); //1 //null-y sarqum a number` 0, u 0+1=1
+console.log(undefined + 1); //NaN //undefinedy number piti sarqi, talis a NaN
 
 console.log(Number("0x77") === 0x77); //true //0x77y tiv e 16akan hamakargum, Number()-ov hanum a ed tivy, u ayd tivy havasar e inqn iren
 console.log(Number("077") === 077); //false //"077"y sarqum a tiv 77, 77 === chi 077
